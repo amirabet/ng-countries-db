13d55cb067be8b739bb39e61202573a7000620e5
Artur Mirabet <arturmirabet@gmail.com>
2024-04-03 18:52:29 +0200

    Publish docs folder

--- countryApp/src/app/app-routing.module.ts
+++ countryApp/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
+
+/*
+*
+* RouterModule.forRoot(routes) => router principal
+* RouterModule.forChild(routes) => routers secundarios
+*
+*/
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
